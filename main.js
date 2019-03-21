f797009e3cdc0afbba34facc3301124f62211039
282Haniwa <282Haniwa@gmail.com>
2019-03-21 15:17:04 +0900

    Create interpreter mode and file select mode.

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
+const fs = require('fs')
+const readlineSync = require('readline-sync')
 const BrainFuck = require('./BrainFuck')
 
-const aBrainFuck = new BrainFuck()
-const result = aBrainFuck.evaluate('+++++++++[>++++++++>+++++++++++>+++>+<<<<-]>.>++.+++++++..+++.>+++++.<<+++++++++++++++.>.+++.------.--------.>+.>+.')
-console.log(result)
+if (process.argv[2] === '-i' || process.argv[2] === '--interpreter') {
+  // インタプリタモード
+  let inputString = ''
+  while (true) {
+    const inputLine = readlineSync.prompt()
+    if (inputLine === 'exit') {
+      break
+    }
+    inputString += inputLine
+    if (inputLine === '') {
+      const aBrainFuck = new BrainFuck()
+      const result = aBrainFuck.evaluate(inputString)
+      console.log(result)
+      inputString = ''
+    }
+  }
+} else {
+  // ファイル指定モード
+  const fileName = process.argv[2]
+  fs.readFile(fileName, 'utf-8', (error, data) => {
+    if (error) {
+      throw error
+    }
+    const aBrainFuck = new BrainFuck()
+    const result = aBrainFuck.evaluate(data)
+    console.log(result)
+  })
+}
