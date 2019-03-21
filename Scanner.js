977e87aac9c1e971deb94100b6dde8b985ebb57a
282Haniwa <282Haniwa@gmail.com>
2019-03-21 13:19:56 +0900

    Fix behavior of scanning unexpected token.

--- Scanner.js
+++ Scanner.js
@@ -7,21 +7,17 @@ class Scanner {
     const tokenArray = []
     let program = string
 
-    try {
-      while (program.length > 0) {
-        const token_number = tokenArray.length
-        Object.entries(this.tokenDefinition).forEach(token => {
-          if (program.indexOf(token[1]) === 0) {
-            program = program.replace(token[1], '')
-            tokenArray.push(token[0])
-          }
-        })
-        if (token_number === tokenArray.length) {
-          throw new Error('パースできない文字列が現れました。');
+    while (program.length > 0) {
+      const token_number = tokenArray.length
+      Object.entries(this.tokenDefinition).forEach(token => {
+        if (program.indexOf(token[1]) === 0) {
+          program = program.replace(token[1], '')
+          tokenArray.push(token[0])
         }
+      })
+      if (token_number === tokenArray.length) {
+        program = program.slice(1)
       }
-    } catch (error) {
-      console.log(e.message);
     }
     return tokenArray
   }
