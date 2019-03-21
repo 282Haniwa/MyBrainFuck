bfe40cc7892748570af8f9b0719caab568c29815
282Haniwa <282Haniwa@gmail.com>
2019-03-21 10:52:40 +0900

    Fix Scanner.

--- Scanner.js
+++ Scanner.js
@@ -1,14 +1,16 @@
-const tokens = require('./tokens')
-
 class Scanner {
-  static scanToken(string) {
+  constructor(tokens) {
+    this.tokenDefinition = tokens
+  }
+
+  scanToken(string) {
     const tokenArray = []
     let program = string
 
     try {
       while (program.length > 0) {
         const token_number = tokenArray.length
-        Object.entries(tokens).forEach(token => {
+        Object.entries(this.tokenDefinition).forEach(token => {
           if (program.indexOf(token[1]) === 0) {
             program = program.replace(token[1], '')
             tokenArray.push(token[0])
@@ -21,6 +23,7 @@ class Scanner {
     } catch (error) {
       console.log(e.message);
     }
+    return tokenArray
   }
 }
 
